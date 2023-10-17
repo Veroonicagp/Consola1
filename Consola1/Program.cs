c2df8920aa62e16ce3b023289b2c488f59784035
Verónica González Pons <91873213+Veronicagnzpns@users.noreply.github.com>
2023-10-17 13:31:09 +0200

    Agregar archivos de proyecto.

---
+++ Consola1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consola1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double PI = 3.14;
+            Console.WriteLine("Introduce la medida del radio");
+            double r = int.Parse(Console.ReadLine());
+
+             double area = PI * Math.Pow(r, 2);
+
+            Console.WriteLine("El area del circulo es " + area);
+        }
+    }
+}
